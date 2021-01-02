a944929215dfbeb808bb79f3b04fcf86b74fde71
Kala Foods <guhan.delta@gmail.com>
2021-01-01 22:05:50 -0500

    Modified the reducer functions to be contained within a switchcase block

--- src/reducer.js
+++ src/reducer.js
@@ -1,22 +1,44 @@
 let lastId = 0;
 
 function reducer(state=[], action){ // Initial state [], to prevent it being set to undefined
-    // if - else or switch - case may be implemented here
-    if(action.type === 'bugAdded'){
-        return[
-            // Take a copy of the state
-            ...state,
-            // Bug object
-            {
-                id: ++lastId,
-                description: action.payload.description,
-                resolved: false
-            }
-        ];
-    }else if(action.type === 'bugRemoved'){
-        return state.filter(bug => bug.id !== action.payload.id);
+    switch(action.type){
+        case "bugAddes":
+            return[
+                // Take a copy of the state
+                ...state,
+                // Bug object
+                {
+                    id: ++lastId,
+                    description: action.payload.description,
+                    resolved: false
+                }
+            ];
+        case "bugRemoved":
+            return state.filter(bug => bug.id !== action.payload.id);
+        default:
+            return state;
     }
-    // When the app is started, the store will be initially undefined, and when Redux calls the reducer, it will-
-    //- pass in undefined as the value for the state. In that case return the state, instead of undefined or null
-    return state;
+    }
+    
 }
+
+// function reducer(state=[], action){ // Initial state [], to prevent it being set to undefined
+//     // if - else or switch - case may be implemented here
+//     if(action.type === 'bugAdded'){
+//         return[
+//             // Take a copy of the state
+//             ...state,
+//             // Bug object
+//             {
+//                 id: ++lastId,
+//                 description: action.payload.description,
+//                 resolved: false
+//             }
+//         ];
+//     }else if(action.type === 'bugRemoved'){
+//         return state.filter(bug => bug.id !== action.payload.id);
+//     }
+//     // When the app is started, the store will be initially undefined, and when Redux calls the reducer, it will-
+//     //- pass in undefined as the value for the state. In that case return the state, instead of undefined or null
+//     return state;
+// }
