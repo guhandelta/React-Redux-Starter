1b73b0b59c31e3a83233622f1b58a4a4bcb9969a
Kala Foods <guhan.delta@gmail.com>
2021-01-01 22:02:31 -0500

    Created teh reducer functions for add and remove bug events/actions

---
+++ src/reducer.js
@@ -0,0 +1,22 @@
+let lastId = 0;
+
+function reducer(state=[], action){ // Initial state [], to prevent it being set to undefined
+    // if - else or switch - case may be implemented here
+    if(action.type === 'bugAdded'){
+        return[
+            // Take a copy of the state
+            ...state,
+            // Bug object
+            {
+                id: ++lastId,
+                description: action.payload.description,
+                resolved: false
+            }
+        ];
+    }else if(action.type === 'bugRemoved'){
+        return state.filter(bug => bug.id !== action.payload.id);
+    }
+    // When the app is started, the store will be initially undefined, and when Redux calls the reducer, it will-
+    //- pass in undefined as the value for the state. In that case return the state, instead of undefined or null
+    return state;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,12 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
+    // Webpack will take all the app files and compress it into app.js---
     filename: "app.js",
+    // ---and place it in /dist
     path: path.resolve(__dirname, "dist")
   },
+  // Webpack Dev Server - Lighweight Server | Instructing it to launch the app on :3000, from /dist
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     port: 3000
